31d61c13e97e84a745f2b8c4e89c1449a756a5c7
wfercanas <fernando.canas@outlook.com>
2021-12-06 10:12:22 -0500

    add/ starting deep copy exercise

---
+++ src/deepCopy.js
@@ -0,0 +1,27 @@
+const obj1 = {
+  a: 'a',
+  b: 'b',
+  c: {
+    d: 'd',
+    e: 'e',
+  },
+
+  editA() {
+    this.a = 'AAAA';
+  },
+};
+
+function isObject(element) {
+  return typeof element === 'object';
+}
+
+function isArray(element) {
+  return Array.isArray(element);
+}
+
+function deepCopy(element) {
+  let elementCopy;
+  const 
+
+  return elementCopy;
+}
