9c92a66fa1a8f54bfe9c47cef30d324cc5e1aa86
wfercanas <fernando.canas@outlook.com>
2021-12-06 10:43:35 -0500

    add/ deep copy an object or array with recursion

--- src/deepCopy.js
+++ src/deepCopy.js
@@ -1,18 +1,18 @@
 const obj1 = {
-  a: 'a',
-  b: 'b',
+  a: "a",
+  b: "b",
   c: {
-    d: 'd',
-    e: 'e',
+    d: "d",
+    e: "e",
   },
 
   editA() {
-    this.a = 'AAAA';
+    this.a = "AAAA";
   },
 };
 
 function isObject(element) {
-  return typeof element === 'object';
+  return typeof element === "object";
 }
 
 function isArray(element) {
@@ -21,7 +21,32 @@ function isArray(element) {
 
 function deepCopy(element) {
   let elementCopy;
-  const 
+  const elementIsArray = isArray(element);
+  const elementIsObject = isObject(element);
+
+  if (elementIsArray) {
+    elementCopy = [];
+  } else if (elementIsObject) {
+    elementCopy = {};
+  } else {
+    return element;
+  }
+
+  for (key in element) {
+    const keyIsArray = isArray(key);
+    const keyIsObject = isObject(element[key]);
+
+    if (keyIsObject && !keyIsArray) {
+      elementCopy[key] = deepCopy(element[key]);
+    } else if (keyIsArray) {
+      elementCopy.push(element[key]);
+    } else {
+      elementCopy[key] = element[key];
+    }
+  }
 
   return elementCopy;
 }
+
+const obj2 = deepCopy(obj1);
+console.log(obj2);
