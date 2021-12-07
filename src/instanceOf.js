d9afd7f663f35dcefc86f6630f50941b2bfabebe
wfercanas <fernando.canas@outlook.com>
2021-12-06 21:20:50 -0500

    add/ instanceof exercise

--- src/instanceOf.js
+++ src/instanceOf.js
@@ -6,14 +6,14 @@ function requiredParam(param) {
   throw new Error(`${param} es obligatorio`);
 }
 
-function LearningPath({ name = requiredParam("name"), courses = [] }) {
+function LearningPath({ name = requiredParam('name'), courses = [] }) {
   this.name = name;
   this.courses = courses;
 }
 
 function Student({
-  name = requiredParam("name"),
-  email = requiredParam("email"),
+  name = requiredParam('name'),
+  email = requiredParam('email'),
   age,
   approvedCourses = [],
   learningPaths = [],
@@ -21,36 +21,37 @@ function Student({
   facebook,
   instagram,
 } = {}) {
-  if (!isArray(learningPaths)) {
-    console.warn("Learning Paths no es un array");
-  } else {
-    for (lp in learningPaths) {
-      if (!(learningPaths[lp] instanceof LearningPath)) {
-        console.warn(`${learningPaths[lp]} no es un verdadero LearningPath`);
-        return;
-      }
-    }
-  }
-
   this.name = name;
   this.email = email;
   this.age = age;
   this.approvedCourses = approvedCourses;
-  this.learningPaths = learningPaths;
   this.socialMedia = {
     twitter,
     facebook,
     instagram,
   };
+
+  if (!isArray(learningPaths)) {
+    console.warn('Learning Paths no es un array');
+  } else {
+    for (lp in learningPaths) {
+      if (!(learningPaths[lp] instanceof LearningPath)) {
+        console.warn(`${learningPaths[lp]} no es un verdadero LearningPath`);
+        return;
+      } else {
+        this.learningPaths = learningPaths;
+      }
+    }
+  }
 }
 
 const web = new LearningPath({
-  name: "Web Development",
+  name: 'Web Development',
 });
 
 const juan = new Student({
-  name: "Juan",
+  name: 'Juan',
   age: 18,
-  email: "juanito@frijolitos.com",
-  twitter: "fjuandc",
+  email: 'juanito@frijolitos.com',
+  twitter: 'fjuandc',
 }); // {}
